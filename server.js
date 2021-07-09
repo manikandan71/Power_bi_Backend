e73ffe0dd82a9a3b4c38bd6c587080c36b0ddc18
Manikandan <57445267+manikandan71@users.noreply.github.com>
2021-07-09 15:36:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,19 +14,19 @@ app.use(cors());
 app.get("/finance", function (req, res) {
 var options = {
   'method': 'GET',
-  'url': 'https://login.microsoftonline.com/00c7bb1f-2bf9-4e47-b448-b16f47190b27/oauth2/token',
+  'url': 'https://login.microsoftonline.com//oauth2/token',
   'headers': {
     'Cookie': 'x-ms-gateway-slice=estsfd; stsservicecookie=estsfd; fpc=AganJvlgzsJAoSoa3tPbWDqtXf4iAQAAAK-CSdgOAAAA'
   },
   formData: {
-    'client_id': '64cc1dd2-624f-4a5c-936b-452473ed4769',
+    'client_id': '',
     'scope': 'openid',
-    'code': '46bb7cc0-001a-4885-9ef6-9aa3cf6b9249',
+    'code': '',
     'redirect_uri': 'http://localhost/5300',
     'grant_type': 'password',
-    'client_secret': 'ZZ45I7Xps3V-k4--3KDUf~yipCE.2Fm.Jz',
-    'username': 'aswathi@ittstar.com',
-    'password': 'AchuNithin2595@',
+    'client_secret': '',
+    'username': '',
+    'password': '',
     'resource': 'https://analysis.windows.net/powerbi/api'
   }
 };
